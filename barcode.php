af198388a430c6bfb06a1df1458e788855d4a1d6
Pranav Kottoli Radhakrishna <52784810+PranavKR66@users.noreply.github.com>
2020-11-15 16:42:32 -0500

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -1,12 +1,31 @@
 <?php
    require 'aws.phar';
-   require ('razrAWS.php');
-   use razrPHP as RAZR;
-   $razr = new RAZR\rDynamo ();
-
-   $hK = array('hashKey', 'S', 'HASH');
-   $rK = array('rangeKey', 'S', 'RANGE');
-   $tput = array(1, 1);
-   $t = $razr->razrTable('[table_name]', $hK, $rK, $tput);
-   echo $t;
 ?>
+<html>
+<head>
+<style>
+p.inline {display: inline-block;}
+span { font-size: 13px;}
+</style>
+<style type="text/css" media="print">
+    @page 
+    {
+        size: auto;   /* auto is the initial value */
+        margin: 0mm;  /* this affects the margin in the printer settings */
+
+    }
+</style>
+</head>
+<body onload="window.print();">
+	<div style="margin-left: 5%">
+		<?php
+		include 'barcode128.php';
+		$email = $_POST['email'];
+		$name  = $_POST['name'];
+		$phone = $_POST['phone'];
+		echo "<p class='inline'><span ><b>Email: $email</b></span>".bar128(stripcslashes($_POST['phone']))."<span ><b>NAME: ".$name." </b><span></p>&nbsp&nbsp&nbsp&nbsp";
+
+		?>
+	</div>
+</body>
+</html>
