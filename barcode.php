fce32bb095a7c9465edd4f82ab35f832fe60f4f1
Pranav Kottoli Radhakrishna <52784810+PranavKR66@users.noreply.github.com>
2020-11-17 00:24:55 -0500

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -55,11 +55,11 @@ span { font-size: 13px;}
 
     		$bucketName = 'bushubbucket';
     		$fileformat = '.png';
-		$s3->putObject(['Bucket' => $bucketName,'Key'    => (string)$index.$fileformat,'Body'   => $imageData,'ACL'    => 'public-read',]);
 
 		$data = ['index' => $index,'email' => $email,'name' => $name,'phone' => $phone,];
 
 		$dynamodb->putItem(['TableName' => $tableName,'Item'      => $marshaler->marshalItem($data)]);
+		$s3->putObject(['Bucket' => $bucketName,'Key'    => (string)$index.$fileformat,'Body'   => $imageData,'ACL'    => 'public-read',]);
 
 		?>
 	</div>
